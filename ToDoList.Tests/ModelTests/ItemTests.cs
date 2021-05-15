3a4538599d6a2018e68e177a52cbd0bd72d42e9d
Nick Hennessy <njhnny@gmail.com>
2021-05-14 18:15:42 -0700

    refactor ItemTests

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -14,6 +14,11 @@ namespace ToDoList.Tests
       Item.ClearAll();
     }
 
+    public ItemTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
